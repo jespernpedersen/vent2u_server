0fca99ddca7a562cb4989c479b6ef2cbed292a80
Jesper Pedersen <jeppenp12@gmail.com>
2020-11-16 12:42:03 +0100

    Endpoint /get/ for classes

--- app/routes/vents.routes.js
+++ app/routes/vents.routes.js
@@ -5,12 +5,14 @@ module.exports = app => {
     const router = require("express").Router();
 
     const vents = require("../controllers/vent.controller");
+    const classes = require("../controllers/class.controller");
 
-    // Read,
+    // Read
     router.get('/', vents.get);
     router.get('/:id', vents.get);
     router.get('/room/:id', vents.getFromRoom);
     router.get('/user/:id', vents.getFromUser);
+    router.get('/classes/:id', classes.get);
 
     // Create, Update
     router.put('/', vents.update);
