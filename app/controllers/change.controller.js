0969341d17beccbb6e875d47c208518108d611ed
Jesper Pedersen <jeppenp12@gmail.com>
2020-12-10 15:29:53 +0100

    Change controller

--- app/controllers/change.controller.js
+++ app/controllers/change.controller.js
@@ -6,31 +6,45 @@ const RequestedChanges = db.requested_changes;
 // const Op = db.Sequelize.Op;
 
 exports.post = (req, res) => {
-    // For every item
-    req.body.forEach((item) => {
-        let options = null;
-        if (item.id == 0 || item.id == 6) {
-            // Get matching info from option table based on string
-            options = 0;
-        } else {
-            options = 1;
-        }
-        try {
-            requestedChange = {
-                // Needs to be auto increment from database, but won't allow null value right now
-                ID: item.id,
-                request_id: item.id,
-                // Should be getting from options table
-                option_id: options
+    RequestGroup = {
+        user_id: 1,
+        vent_id: 1,
+        status_id: 0
+    }
+    ChangeRequests.create(RequestGroup, { isNewRecord: true }).complete(function(err, result) {
+            if (err) {
+                callback(0);
+            } else {
+                callback(result.id); // This is generated primary key
             }
-            RequestedChanges.create(requestedChange)
-                // If error
-            if (!item.id) throw new exception();
-        } catch {
-            console.log("Item ID cannot be null " + item.id);
-            return;
-        }
-    });
+        })
+        /*
+        // For every item
+        req.body.forEach((item) => {
+            let options = null;
+            if (item.id == 0 || item.id == 6) {
+                // Get matching info from option table based on string
+                options = 0;
+            } else {
+                options = 1;
+            }
+            try {
+                requestedChange = {
+                    // Needs to be auto increment from database, but won't allow null value right now
+                    ID: item.id,
+                    request_id: item.id,
+                    // Should be getting from options table
+                    option_id: options
+                }
+                RequestedChanges.create(requestedChange)
+                    // If error
+                if (!item.id) throw new exception();
+            } catch {
+                console.log("Item ID cannot be null " + item.id);
+                return;
+            }
+        });
+        */
 }
 
 exports.get = (req, res) => {
