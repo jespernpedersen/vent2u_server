ca23bc8e2cbbbe7f3376df1d82afcafb3926448b
diogo <diog0047@edu.easj.dk>
2020-12-14 12:21:58 +0000

    Heroku deployment

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 require("./app/routes/vent2u.routes")(app);
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}.`);
 });
