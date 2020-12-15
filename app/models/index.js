5c7af49b839ab3cc43fe2a10cf178be0336400df
diogo <diog0047@edu.easj.dk>
2020-12-15 14:14:55 +0000

    Changed database configurations

--- app/models/index.js
+++ app/models/index.js
@@ -1,13 +1,10 @@
 require("dotenv").config();
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(process.env.DB_CONN_STRING+"?sslmode=require", {
+const sequelize = new Sequelize(process.env.DB_CONN_STRING, {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
     protocol: process.env.DB_PROTOCOL,
-    dialectOptions: {
-        ssl: false
-    },
     port: process.env.DB_PORT,
     syncOnAssociation: false
 });
