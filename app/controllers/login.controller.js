15a786a4ae3a75d9833aa9eca09645127110207a
diogo <diog0047@edu.easj.dk>
2020-12-15 10:19:48 +0000

    fixed some problems

--- app/controllers/login.controller.js
+++ app/controllers/login.controller.js
@@ -1,17 +1,17 @@
 const db = require("../models");
-require('dotenv').config()
+
 
 exports.login = (req, res) => {
 
-    import 'connection.js';
-
     const query = `call login(?, ?)`;
-    sequelize.query(query, {
-        replacements: [req.body.email, req.body.password],
-        type: sequelize.QueryTypes.SELECT
-    }).then(data => {
-        res.send(data);
-    }).catch(err => {
-        res.status(500).send(err);
-    });
+
+    db.query(query, {
+            replacements: [req.body.email, req.body.password],
+            type: sequelize.QueryTypes.SELECT
+        }).then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send(err);
+        });
 }
--- app/models/index.js
+++ app/models/index.js
@@ -1,5 +1,12 @@
+require("dotenv").config();
+
 const Sequelize = require("sequelize");
-import 'connection.js';
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
+    syncOnAssociation: false
+});
 
 const db = {};
 
